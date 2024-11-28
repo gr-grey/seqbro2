17c74761fbf0eb5c1ec8126b6d81ae0a32ca0f9d
Jian Zhou <jzhou@gmail.com>
2024-11-28 11:08:36 -0500

    left and right button to move sequence above sequence box

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,31 @@ const MainContent = () => {
     return () => { window.removeEventListener("wheel", handleWheel); };
   }, []);
 
+  // use buttons to scroll left and right
+  const scrollLeft = () => {
+    if (sequenceBoxRef.current) { sequenceBoxRef.current.scrollLeft -= 100; }
+  };
+  const scrollRight = () => {
+    if (sequenceBoxRef.current) { sequenceBoxRef.current.scrollLeft += 100; }
+  };
+
+  // left < and right > buttons with continuous scrolling
+  const [scrollInterval, setScrollInterval] = useState(null);
+  const startScrolling = (direction) => {
+    if (!scrollInterval) {
+      const interval = setInterval(() => {
+        if (sequenceBoxRef.current) { sequenceBoxRef.current.scrollLeft += direction; } // use positive dir to scroll right, neg to scroll left
+      }, 50); // adjust interval for smoothness
+      setScrollInterval(interval);
+    }
+  };
+  const stopScrolling = () => {
+    if (scrollInterval) {
+      clearInterval(scrollInterval);
+      setScrollInterval(null);
+    }
+  };
+
   return (
     <div className="flex h-screen">
       {/* Left side of screen 1/4 or max-80 */}
@@ -39,6 +64,25 @@ const MainContent = () => {
 
       {/* Right side */}
       <div className="w-3/4 flex-grow p-2">
+        <div className="flex mb-2">
+          <button
+            onMouseDown={() => startScrolling(-30)} // scroll left
+            onMouseUp={stopScrolling}
+            onMouseLeave={stopScrolling}
+            className="px-2 py-1 mr-2 bg-gray-50 border rounded-lg hover:bg-gray-200"
+          >
+            &lt; {/* Left Arrow */}
+          </button>
+          <button
+            onMouseDown={() => startScrolling(30)} // scroll right
+            onMouseUp={stopScrolling}
+            onMouseLeave={stopScrolling}
+            className="px-2 py-1 mr-2 bg-gray-50 border rounded-lg hover:bg-gray-200"
+          >
+            &gt; {/* Right Arrow */}
+          </button>
+
+        </div>
         <SequenceBox />
         <TrackValues />
       </div>
